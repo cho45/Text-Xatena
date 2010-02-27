d7ad0073149b1c764903e386ce448bb211531eeb
cho45 <cho45@lowreal.net>
2010-02-27 13:41:33 +0900

    init

---
+++ lib/Text/HatenaX.pm
@@ -0,0 +1,33 @@
+package Text::HatenaX;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+Text::HatenaX -
+
+=head1 SYNOPSIS
+
+  use Text::HatenaX;
+
+=head1 DESCRIPTION
+
+Text::HatenaX is
+
+=head1 AUTHOR
+
+cho45 E<lt>cho45@lowreal.netE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
