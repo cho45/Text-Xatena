163aa4eacb265645f50f9adfcf76f1b4b0394c0b
cho45 <cho45@lowreal.net>
2010-02-28 07:23:01 +0900

    ol/ul 記法

--- lib/Text/HatenaX/Node/List.pm
+++ lib/Text/HatenaX/Node/List.pm
@@ -6,17 +6,67 @@ use base qw(Text::HatenaX::Node);
 
 sub parse {
     my ($class, $s, $parent, $stack) = @_;
-    for my $l (qw/- + :/) {
+    for my $l (qw/- +/) {
         my $l = quotemeta $l;
         if ($s->scan(qr/^$l/)) {
-            my $a = $class->new([ $s->matched->[0] ]);
+            my $node = $class->new([ $s->matched->[0] ]);
             until ($s->eos || !$s->scan(qr/^$l/)) {
-                push @$a, $s->matched->[0];
+                push @$node, $s->matched->[0];
             }
-            push @$parent, $a;
+            push @$parent, $node;
             return 1;
         }
     }
+}
+
+sub as_struct {
+    my ($self) = @_;
+
+    my $stack = [ { children => [] } ];
+    my $children = $self->children;
+
+    for my $line (@$children) {
+        my ($symbol, $text) = ($line =~ /^([-+]+)(.+)$/);
+        my $level = length($symbol);
+        pop @$stack while (scalar @$stack > $level * 2);
+        while (scalar @$stack < $level * 2) {
+            my $node = +{
+                name     => (substr($line, $level - 1, 1) eq '+' ? 'ol' : 'ul'),
+                children => []
+            };
+
+            push @{ $stack->[-1]->{children} }, $node if @$stack;
+            push @$stack, $node;
+        }
+
+        my $node = +{
+            name     => 'li',
+            children => [ $text ]
+        };
+        push @{ $stack->[-1]->{children} }, $node;
+        push @$stack, $node;
+    }
+
+    $stack->[1];
+}
+
+sub as_html {
+    my ($self, %opts) = @_;
+    $self->_as_html($self->as_struct, %opts);
+}
+
+sub _as_html {
+    my ($self, $obj, %opts) = @_;
+    my $ret = "<" . $obj->{name} . ">\n";
+    for my $child (@{ $obj->{children} }) {
+        if (ref($child)) {
+            $ret .= $self->_as_html($child, %opts);
+        } else {
+            $ret .= $child;
+        }
+    }
+    $ret .= "</" . $obj->{name} . ">\n";
+    $ret;
 }
 
 
---
+++ t/01_list.t
@@ -0,0 +1,66 @@
+use strict;
+use warnings;
+use lib 't/lib';
+use Text::HatenaX::Test;
+
+plan tests => 1 * blocks;
+
+run_html;
+
+
+__END__
+
+=== test
+--- input
+- 1
+- 2
+- 3
+--- expected
+<ul>
+    <li>1</li>
+    <li>2</li>
+    <li>3</li>
+</ul>
+
+=== test
+--- input
+- 1
+- 2
+-- 2.1
+-- 2.2
+--+ 2.2.3
+- 3
+--- expected
+<ul>
+    <li>1</li>
+    <li>2
+        <ul>
+            <li>2.1</li>
+            <li>2.2
+                <ol>
+                    <li>2.2.3</li>
+                </ol>
+            </li>
+        </ul>
+    </li>
+    <li>3</li>
+</ul>
+
+=== test
+--- input
+- 1
+- 2
+-+ 2.1
+-+ 2.2
+- 3
+--- expected
+<ul>
+    <li>1</li>
+    <li>2
+        <ol>
+            <li>2.1</li>
+            <li>2.2</li>
+        </ol>
+    </li>
+    <li>3</li>
+</ul>
