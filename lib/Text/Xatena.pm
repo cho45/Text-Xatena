ac5a833492bc82ced2df02ba661bcaba7245ed44
cho45 <cho45@lowreal.net>
2010-03-23 02:50:11 +0900

    Checking in changes prior to tagging of version 0.03.  Changelog diff is:

--- lib/Text/Xatena.pm
+++ lib/Text/Xatena.pm
@@ -9,7 +9,7 @@ use Text::Xatena::Node;
 use Text::Xatena::Node::Root;
 use Text::Xatena::Inline;
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 our $SYNTAXES = [
     'Text::Xatena::Node::SuperPre',
