0eac1fceb41e6a5da53c77181def5c3c3a93d3ad
cho45 <cho45@lowreal.net>
2011-12-01 11:47:50 +0900

    Checking in changes prior to tagging of version 0.16.
    Changelog diff is:

--- lib/Text/Xatena.pm
+++ lib/Text/Xatena.pm
@@ -10,7 +10,7 @@ use Text::Xatena::Node::Root;
 use Text::Xatena::Inline;
 use Text::Xatena::Util;
 
-our $VERSION = '0.15';
+our $VERSION = '0.16';
 
 our $SYNTAXES = [
     'Text::Xatena::Node::SeeMore',
